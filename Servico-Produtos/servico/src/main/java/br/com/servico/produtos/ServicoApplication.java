61754bb1681829f9aca751ec4a07074af76145ff
marceloflp <mfelipedasilva2020@gmail.com>
2026-05-11 20:13:01 -0300

    Criacao do projeto

---
+++ Servico-Produtos/servico/src/main/java/br/com/servico/produtos/ServicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.servico.produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoApplication.class, args);
+	}
+
+}
---
+++ Servico-Produtos/servico/src/test/java/br/com/produtos/servico/ServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.produtos.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
