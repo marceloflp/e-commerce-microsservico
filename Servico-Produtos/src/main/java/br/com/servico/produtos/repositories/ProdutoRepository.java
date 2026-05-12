724fd6c278e93a7968d8272ac73a44381ed07793
marceloflp <mfelipedasilva2020@gmail.com>
2026-05-11 21:27:20 -0300

    Criacao do repositorio ProdutoRepository

---
+++ Servico-Produtos/src/main/java/br/com/servico/produtos/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.servico.produtos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.servico.produtos.entities.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+}
