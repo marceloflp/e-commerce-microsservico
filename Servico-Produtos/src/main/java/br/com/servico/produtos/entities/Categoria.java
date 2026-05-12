5c1fa8681060653a49e09754bbc5351b9d2b4ab2
marceloflp <mfelipedasilva2020@gmail.com>
2026-05-11 21:20:52 -0300

    Criacao da entidade categoria

---
+++ Servico-Produtos/src/main/java/br/com/servico/produtos/entities/Categoria.java
@@ -0,0 +1,58 @@
+package br.com.servico.produtos.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCategoria;
+	private String nome;
+	
+	public Categoria(Long idCategoria, String nome) {
+		super();
+		this.idCategoria = idCategoria;
+		this.nome = nome;
+	}
+	
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(Long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(idCategoria);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(idCategoria, other.idCategoria);
+	}
+
+}
