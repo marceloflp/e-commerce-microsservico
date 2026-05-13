def53eeb1d9e56ec3328e584ae917f55d5d88811
marceloflp <mfelipedasilva2020@gmail.com>
2026-05-13 16:18:45 -0300

    Criacao da entidade pedido no servico de pedido

---
+++ Servico-Pedidos/src/main/java/com/servico/pedidos/entities/Pedido.java
@@ -0,0 +1,90 @@
+package com.servico.pedidos.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.servico.pedidos.enums.Status;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idPedido;
+	private String nomeCliente;
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataPedido;
+	private Status status;
+	private Double valorTotal;
+	
+	public Pedido() {}
+	
+	public Pedido(Long idPedido, String nomeCliente, LocalDate dataPedido, Status status, Double valorTotal) {
+		super();
+		this.idPedido = idPedido;
+		this.nomeCliente = nomeCliente;
+		this.dataPedido = dataPedido;
+		this.status = status;
+		this.valorTotal = valorTotal;
+	}
+	
+	public Long getIdPedido() {
+		return idPedido;
+	}
+	public void setIdPedido(Long idPedido) {
+		this.idPedido = idPedido;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public LocalDate getDataPedido() {
+		return dataPedido;
+	}
+	public void setDataPedido(LocalDate dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(idPedido);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(idPedido, other.idPedido);
+	}
+	
+	
+}
