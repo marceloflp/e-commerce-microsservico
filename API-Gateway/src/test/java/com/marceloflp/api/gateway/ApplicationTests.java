81a4b63526778f51db502fd5605a46e579133d44
marceloflp <mfelipedasilva2020@gmail.com>
2026-09-04 13:10:14 -0300

    Criando API Gateway para roteamento de rotas

--- API-Gateway/src/test/java/com/marceloflp/api/gateway/ApplicationTests.java
+++ API-Gateway/src/test/java/com/marceloflp/api/gateway/ApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.produtos.servico;
+package com.marceloflp.api.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServicoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
